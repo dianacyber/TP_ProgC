c811ceadca02369b362528b68aabbdd5c2d1220a
dianacyber <dianagakou@gmail.com>
2026-09-18 13:33:47 +0000

    tp1.2

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+#include <math.h>
 
 int main() {
-double pi = 3.1416;
-double r = 6;  
-    printf("L'air du cercle = %f", pi*(r*r));
-   printf("Le périmètre du cercle = %f", pi*(r+r));
+    double rayon = 6.0;
+    double aire;
+    double perimetre;
+
+    aire = M_PI * rayon * rayon;
+    perimetre = 2 * M_PI * rayon;
+
+    printf("Rayon : %.2f\n", rayon);
+    printf("Aire du cercle : %.2f\n", aire);
+    printf("Perimetre du cercle : %.2f\n", perimetre);
+
+    return 0;
 }
 
